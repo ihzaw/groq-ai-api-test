c6ed4b671f2b3698aea7757085e9affef62933ac
ihzatw <ext_ihzaanantama.kharisma@malaysiaairlines.com>
2024-10-18 19:03:54 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const Groq = require("groq-sdk");
+
+const PORT = 3000;
+const app = express();
+
+require("dotenv").config();
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+const getPromptTarget = (target) => {
+  if (target === 'sis') {
+    return process.env.SIS_PROMPT
+  }
+  if (target === 'cer') {
+    return process.env.CER_PROMPT
+  }
+
+  return `${process.env.GENERIC_PROMPT} ini tujuannya untuk ${target}. refer ${target} jika bisa` 
+}
+
+async function getGroqChatCompletion(target) {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: getPromptTarget(target),
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+}
+
+app.get("/shortcut-wa/:target", async (req, res) => {
+  const { target } = req.params
+  const response = await getGroqChatCompletion(target);
+
+  if (response?.choices[0]?.message?.content) {
+    const rawMessage = response.choices[0].message.content;
+    res.json({ text: rawMessage });
+    return
+  }
+
+  res.json({ text: "Failed to create message, don't taru kira" });
+});
+
+app.get("/shortcut-wa/:target", async (req, res) => {
+  const { target } = req.params
+  const response = await getGroqChatCompletion(target);
+
+  if (response?.choices[0]?.message?.content) {
+    const rawMessage = response.choices[0].message.content;
+    res.json({ text: rawMessage });
+  }
+
+  res.json({ text: "Failed to create message, don't taru kira" });
+});
+
+app.get("/", async (req, res) => {
+  res.json({ message: "API working" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
